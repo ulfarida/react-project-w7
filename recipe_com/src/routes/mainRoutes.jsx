5172db0d26d41e26af0e35ca1adaf34d2f504c0e
al7262 <alta5@alta5>
2020-01-09 17:53:25 +0700

    header finished

--- recipe_com/src/routes/mainRoutes.jsx
+++ recipe_com/src/routes/mainRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import Login from '../pages/login'
+import Main from '../pages/main'
 
 import { Provider } from 'unistore/react';
 import { store } from '../store/store';
@@ -12,6 +13,7 @@ const Mainroute = () => {
         <Provider store={store}>
             <BrowserRouter>
                 <Switch>
+                    <Route exact path='/main' component={Main} />
                     <Route exact path='/login' component={Login} />
                 </Switch>
             </BrowserRouter>
